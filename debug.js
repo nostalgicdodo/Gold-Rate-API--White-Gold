5b750f7ef9488ef39dc15c4da684ef63b3ff7ca2
adi <8440761+nostalgicdodo@users.noreply.github.com>
2026-03-27 14:24:37 +0530

    debug.js: No idea what this is

--- debug.js
+++ debug.js
@@ -3,10 +3,26 @@ let rootDir = __dirname
 
 let GoldRate = require( `${ rootDir }/lib/models/gold-rate.js` )
 
+let axios = require( "axios" )
+
 ;( async function main () {
 
-	let goldRates = await GoldRate.getRelevantRatesFromTheDay()
-	console.log( goldRates )
+	const httpClient = axios.create( {
+		headers: { "Cache-Control": "no-store" },
+		timeout: 5000
+	} );
+	httpClient.defaults.headers.common[ "User-Agent" ] = "BFS";
+
+	// let goldRate = await GoldRate.fetchCurrentLiveRate()
+	let response
+	try {
+		response = await httpClient.get( "http://nammauzhavan.com/aditya" );
+	}
+	catch ( e ) {
+		console.log( e.message )
+		return
+	}
+	console.log( response.data )
 
 	process.exit( 0 )
 
